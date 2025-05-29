961afcb895476fe26b1752dfa3279899f391eced
IrfanSidiq <irvansidik64@gmail.com>
2025-05-29 13:43:58 +0700

    feat: abstract class layer

---
+++ src/layer.py
@@ -0,0 +1,25 @@
+from abc import ABC, abstractmethod
+from typing import List, Tuple, Optional
+from tensor import Tensor
+
+
+class Layer(ABC):
+    """
+    Abstract base class for all layers.
+    """
+    def __init__(self):
+        self.weights: List[Tensor] = []
+        self.output: Optional[Tensor] = None
+
+    @abstractmethod
+    def forward(self, input: Tensor) -> Tensor:
+        """
+        Performs the forward pass of the layer.
+        """
+        pass
+
+    def get_parameters(self) -> List[Tensor]:
+        """
+        Returns all learnable parameters (Tensors) of this layer.
+        """
+        return self.weights
